2b90a48849ee9b4662be9e88aad30f9ff59a3c7d
Isaac Bowen <ikebowen@gmail.com>
2024-05-16 11:52:08 -0700

    github/fly/etc

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root 'chats#index'
   post 'chats/message', to: 'chats#message'
 
-  # Health check route
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "__healthcheck" => "rails/health#show", as: :rails_health_check
 end
