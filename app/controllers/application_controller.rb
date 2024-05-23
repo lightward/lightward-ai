3f6020b7d3fa7046e3791e6b55d7300e258ae54a
Isaac Bowen <ikebowen@gmail.com>
2024-05-23 09:19:46 -0500

    disable newrelic browser monitoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # no user monitoring pls
+  # it's buggy (users have reported js errors that are resolved to this client) and also pretty invasive, privacy-wise
+  newrelic_ignore_enduser
 end
