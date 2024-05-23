43cf01521db96c5175591630558b189281528a7b
Isaac Bowen <ikebowen@gmail.com>
2024-05-23 12:51:35 -0500

    disable csrf; save users the headache (#78)
    these are long sessions
    
    this deserves to be thought about, but I'm content with our exposure. we don't use cookies for anything.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,7 @@ class ApplicationController < ActionController::Base
   # no user monitoring pls
   # it's buggy (users have reported js errors that are resolved to this client) and also pretty invasive, privacy-wise
   newrelic_ignore_enduser
+
+  # skipping csrf because right now we're a tiny little js app and I don't think this is needed
+  skip_before_action :verify_authenticity_token
 end
