e628803472781c98397ca9146f68baa4249f811f
Isaac Bowen <ikebowen@gmail.com>
2024-06-01 09:25:32 -0500

    spec fix

--- spec/lib/prompts/helpscout_spec.rb
+++ spec/lib/prompts/helpscout_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe(Helpscout) do
     ENV["HELPSCOUT_WEBHOOK_SECRET_KEY"] = "fake_webhook_secret_key"
     ENV["HELPSCOUT_APP_ID"] = "fake_app_id"
     ENV["HELPSCOUT_APP_SECRET"] = "fake_app_secret"
+    ENV["HELPSCOUT_USER_ID"] = "fake_user_id"
   end
 
   describe ".fetch_conversation" do
