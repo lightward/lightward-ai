48bfebf64971b5c0da7949cefa3df1c8a1d803e1
Isaac Bowen <ikebowen@gmail.com>
2024-07-06 12:16:13 -0500

    adjustments (#246)

--- app/javascript/src/chat.js
+++ app/javascript/src/chat.js
@@ -282,6 +282,7 @@ export const initChat = () => {
   function initializeConsumer(streamId) {
     sequenceQueue = [];
     currentSequenceNumber = 0;
+    let isProcessing = false; // Flag to indicate if the queue is being processed
 
     subscription = consumer.subscriptions.create(
       { channel: 'StreamChannel', stream_id: streamId },
@@ -294,14 +295,8 @@ export const initChat = () => {
           if (data && typeof data.sequence_number === 'number') {
             sequenceQueue.push(data);
             sequenceQueue.sort((a, b) => a.sequence_number - b.sequence_number);
-
-            while (
-              sequenceQueue.length &&
-              sequenceQueue[0].sequence_number === currentSequenceNumber
-            ) {
-              const message = sequenceQueue.shift();
-              processMessage(message);
-              currentSequenceNumber++;
+            if (!isProcessing) {
+              processQueue();
             }
           } else {
             console.error(
@@ -313,11 +308,36 @@ export const initChat = () => {
       }
     );
 
-    setTimeout(() => {
-      if (currentSequenceNumber < sequenceQueue[0]?.sequence_number) {
-        handleTimeoutError();
+    let timeoutId;
+
+    function resetTimeout() {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(handleTimeoutError, TIMEOUT_MS);
+    }
+
+    function processQueue() {
+      if (
+        sequenceQueue.length &&
+        sequenceQueue[0].sequence_number === currentSequenceNumber
+      ) {
+        isProcessing = true; // Set the flag to indicate processing has started
+        const message = sequenceQueue.shift();
+        const randomDelay = Math.floor(Math.random() * 150) + 50;
+
+        setTimeout(() => {
+          currentSequenceNumber++;
+          processMessage(message);
+          processQueue();
+        }, randomDelay);
+
+        resetTimeout();
+      } else {
+        isProcessing = false; // Set the flag to indicate processing has finished
+        clearTimeout(timeoutId); // Clear the timeout when processing is complete
       }
-    }, TIMEOUT_MS);
+    }
+
+    resetTimeout();
   }
 
   function handleTimeoutError() {
