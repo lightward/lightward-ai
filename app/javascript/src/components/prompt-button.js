72ff084b8de93f648ff99c1e02010835a4828c70
Isaac Bowen <ikebowen@gmail.com>
2024-10-14 14:27:37 -0500

    "we're all wiggles" -Alan Watts

--- app/javascript/src/components/prompt-button.js
+++ app/javascript/src/components/prompt-button.js
@@ -2,10 +2,34 @@ export class PromptButtonComponent extends HTMLElement {
   constructor() {
     super();
 
-    // propagate clicks
+    // Propagate clicks
     this.shadowRoot.querySelector('button').addEventListener('click', () => {
       this.dispatchEvent(new CustomEvent('prompt-button-click'));
     });
+
+    // Start the wiggle animation
+    this.startWiggle();
+  }
+
+  startWiggle() {
+    const button = this.shadowRoot.querySelector('button');
+
+    const wiggle = () => {
+      button.classList.add('wiggle');
+
+      // Remove the 'wiggle' class after the animation duration
+      setTimeout(() => {
+        button.classList.remove('wiggle');
+      }, 1000); // Duration of the wiggle animation in milliseconds
+
+      // Schedule the next wiggle at a random time between 2s and 7s
+      const nextWiggle = Math.random() * 5000 + 2000;
+      setTimeout(wiggle, nextWiggle);
+    };
+
+    // Start the first wiggle after a random delay up to 5s
+    const initialDelay = Math.random() * 5000;
+    setTimeout(wiggle, initialDelay);
   }
 }
 
