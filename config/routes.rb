cc27807eb33aea139651314e83de22a3841ae93d
Isaac Bowen <ikebowen@gmail.com>
2024-10-14 14:53:09 -0500

    release (as in remove) the users/buttons layer entirely

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :buttons, only: [:index, :new, :create, :show, :edit, :update] do
-    get :index_archived, on: :collection, path: "archived"
-    post :archive, on: :member
-    post :unarchive, on: :member
-  end
-
-  resource :user, path: "account", only: [:show, :update]
-
   mount GoodJob::Engine => "good_job"
 
   # Root route
@@ -20,9 +12,4 @@ Rails.application.routes.draw do
 
   # Webhook endpoints
   post "webhooks/helpscout", to: "helpscout#receive"
-
-  # Google auth
-  get "login" => "sessions#new", as: :login
-  get "login/create" => "sessions#create", as: :create_login
-  get "logout" => "sessions#destroy", as: :logout
 end
