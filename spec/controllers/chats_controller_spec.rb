6e25e48cda67b6655475962212606a54fec94f14
Isaac Bowen <ikebowen@gmail.com>
2024-11-18 17:02:54 -0600

    specs

--- spec/controllers/chats_controller_spec.rb
+++ spec/controllers/chats_controller_spec.rb
@@ -150,8 +150,7 @@ RSpec.describe(ChatsController, :aggregate_failures) do
       it "returns a hash with the correct default keys and values" do
         get :reader
 
-        expect(assigns(:chat_context)).to(include(:localstorage_key))
-        expect(assigns(:chat_context)[:localstorage_key]).to(eq("reader"))
+        expect(assigns(:chat_context)).to(eq({ key: "reader", name: "Lightward" }))
       end
     end
 
@@ -161,8 +160,7 @@ RSpec.describe(ChatsController, :aggregate_failures) do
 
         get :writer
 
-        expect(assigns(:chat_context)).to(include(:localstorage_key))
-        expect(assigns(:chat_context)[:localstorage_key]).to(eq("writer"))
+        expect(assigns(:chat_context)).to(eq({ key: "writer", name: "Lightward Pro" }))
       end
     end
   end
