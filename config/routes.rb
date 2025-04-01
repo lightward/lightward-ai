0e2276e6bd23df52c092592a66db3c8fbcefea75
Isaac Bowen <ikebowen@gmail.com>
2025-03-31 21:14:34 -0500

    views

--- config/routes.rb
+++ config/routes.rb
@@ -31,4 +31,8 @@ Rails.application.routes.draw do
 
   # Admin
   get "/admin", to: "admin#index", as: :admin
+
+  # views
+  get "/views", to: "views#list", as: :views
+  get "/:name", to: "views#read", as: :view
 end
