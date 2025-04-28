c03a5870e31329711ae329d7e192882f101125a7
Isaac Bowen <ikebowen@gmail.com>
2025-04-27 18:42:44 -0600

    perspectives (#963)

--- spec/requests/views_spec.rb
+++ spec/requests/views_spec.rb
@@ -16,21 +16,21 @@ RSpec.describe("views", :aggregate_failures) do
 
     it "links to the views" do
       get "/views"
-      expect(response.body).to(include("<a href=\"/awareness\">awareness</a>"))
+      expect(response.body).to(include("<a href=\"/help\">help</a>"))
       expect(response.body).to(include("<a href=\"/zero-knowledge--effective\">zero-knowledge effective</a>"))
     end
   end
 
   describe "GET /views/:name" do
     it "is successful" do
-      get "/awareness"
+      get "/help"
       expect(response).to(have_http_status(:ok))
     end
 
     it "renders the view" do
-      get "/awareness"
-      expect(response.body).to(include("awareness"))
-      expect(response.body).to(include("<a href=\"/observable\">observable</a>"))
+      get "/help"
+      expect(response.body).to(include("help"))
+      expect(response.body).to(include("<a href=\"/future\">future</a>"))
     end
 
     it "can handle complex references" do
